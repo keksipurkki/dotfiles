48d075991ed3ef5bf1f47f0fa2083286f443c6ac
Elias Toivanen <elias.toivanen@valuemotive.com>
2018-11-16 14:29:09 +0200

    Add an utility to browse file revisions across commits

--- cli-tools/functions.sh
+++ cli-tools/functions.sh
@@ -343,6 +343,10 @@ function git_sed
   git grep -l "$1" | xargs sed -i '' -e "s|$1|$2|g"
 }
 
+function git_browse 
+{
+  bash $(dirname $BASH_SOURCE)/git_browse $@
+}
 
 # http://stackoverflow.com/a/16509364/4301632
 function fast_chr
