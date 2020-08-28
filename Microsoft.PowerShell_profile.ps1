47c6bfa45f648e2c9d6fbe5067d963ec4e775723
Elias Toivanen <elias.a.toivanen@gmail.com>
2020-08-28 23:41:13 +0300

    PowerShell tweaks

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,29 +1,37 @@
 # Aliases
 function vim { mvim -v $args }
-function vi { mvim --remote-tab-silent $args }
+function vi {
+  if ($args.count) {
+    $args=@("--remote-tab-silent" ) + $args
+  }
+  mvim $args
+}
 
 # Readline
 $PSReadLineOptions = @{
-    Colors = @{
-        Command = "`e[1;30m"
-        ContinuationPrompt = "`e[0;30m"
-        Default = "`e[0;30m"
-        Keyword = "`e[0;30m"
-        Number = "`e[0;30m"
-        Operator = "`e[0;30m"
-        Parameter = "`e[0;30m"
-        Selection = "`e[0;30m"
-        Variable = "`e[0;30m"
-        Type = "`e[0;30m"
-        Member = "`e[30m"
-        Comment = "`e[38;5;250m"
-        Emphasis = "`e[96m"
-        Error = "`e[91m"
-        String = "`e[36m"
-    }
+  Colors = @{
+    Command = "`e[1;30m"
+      ContinuationPrompt = "`e[0;30m"
+      Default = "`e[0;30m"
+      Keyword = "`e[0;30m"
+      Number = "`e[0;30m"
+      Operator = "`e[0;30m"
+      Parameter = "`e[0;30m"
+      Selection = "`e[0;30m"
+      Variable = "`e[0;30m"
+      Type = "`e[0;30m"
+      Member = "`e[30m"
+      Comment = "`e[38;5;250m"
+      Emphasis = "`e[96m"
+      Error = "`e[91m"
+      String = "`e[36m"
+  }
 }
 
 Set-PSReadLineOption @PSReadLineOptions
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 # Posh git
 Import-Module posh-git
